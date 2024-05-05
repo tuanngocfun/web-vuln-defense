c5851d6e1789b457e386aa4d355400edeadd8f0e
tuanngocfun <tuanngoccs50@gmail.com>
2024-05-05 19:56:55 +0700

    reflection adding...

---
+++ Microsoft/Reflection/ReflectionExtensions.cs
@@ -0,0 +1,31 @@
+﻿namespace Microsoft.Reflection
+{
+    using System;
+    using System.Reflection;
+    using System.Runtime.CompilerServices;
+
+    internal static class ReflectionExtensions
+    {
+        public static System.Reflection.Assembly Assembly(this Type type) => 
+            type.Assembly;
+
+        public static Type BaseType(this Type type) => 
+            type.BaseType;
+
+        public static TypeCode GetTypeCode(this Type type) => 
+            Type.GetTypeCode(type);
+
+        public static bool IsAbstract(this Type type) => 
+            type.IsAbstract;
+
+        public static bool IsEnum(this Type type) => 
+            type.IsEnum;
+
+        public static bool IsSealed(this Type type) => 
+            type.IsSealed;
+
+        public static bool ReflectionOnly(this System.Reflection.Assembly assm) => 
+            assm.ReflectionOnly;
+    }
+}
+
