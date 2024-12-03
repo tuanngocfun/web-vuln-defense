269a75142752d14a8827465b6496c95b3e64a588
tuanngocfun <tuanngoccs50@gmail.com>
2024-12-03 09:16:39 +0000

    - Template for homepage () and 404 error page (). - Cat and dog meme views (, ).

---
+++ resources/views/dog.digibusiness.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Dog</title>
+</head>
+
+<body>
+    <img src="{{ assets('images/dog-meme-1.webp') }}" alt="A dog forgot what he was trying to say"/>
+</body>
+
+</html>
---
+++ resources/views/not-found.digibusiness.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Page Not Found</title>
+    <link rel="icon" href="{{ favicon('cropped-Favicon.png') }}">
+</head>
+
+<body>
+    <h1 style="text-align: center;"> 404 Page Not Found </h1>
+</body>
+
+</html>
