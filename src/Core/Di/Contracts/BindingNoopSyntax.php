c447426ed4f94ae69d510aa7dde89d8a753773b3
tuanngocfun <tuanngoccs50@gmail.com>
2024-12-07 16:58:25 +0000

    comment for usage

--- src/Core/Di/Contracts/BindingNoopSyntax.php
+++ src/Core/Di/Contracts/BindingNoopSyntax.php
@@ -1,4 +1,17 @@
 <?php
 namespace App\Core\Di\Contracts;
 
+/**
+ * Interface BindingNoopSyntax
+ *
+ * @description
+ * Một interface rỗng dùng cho các ánh xạ không cần định nghĩa phạm vi.
+ * Ví dụ: Khi ánh xạ một giá trị cố định, không cần phải định nghĩa Transient hay Singleton Scope.
+ *
+ * @example
+ * ```php
+ * $binding = $container->bind('app.name')->toConstant('MyApplication');
+ * // Không cần gọi inTransientScope() hay inSingletonScope().
+ * ```
+ */
 interface BindingNoopSyntax {}
