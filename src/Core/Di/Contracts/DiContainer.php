9d4ab89f8f64d6b07e7b469117d8e17c80123a2b
tuanngocfun <tuanngoccs50@gmail.com>
2024-12-09 17:00:11 +0000

    update docs

--- src/Core/Di/Contracts/DiContainer.php
+++ src/Core/Di/Contracts/DiContainer.php
@@ -1,6 +1,18 @@
 <?php
 namespace App\Core\Di\Contracts;
 
+/**
+ * Interface DiContainer
+ *
+ * @description
+ * Định nghĩa các phương thức cho DI Container để quản lý các gắn kết (bindings) và lấy đối tượng (resolve).
+ *
+ * @example
+ * ```php
+ * $container->bind('my.service')->to(MyService::class)->inSingletonScope();
+ * $service = $container->get('my.service');
+ * ```
+ */
 interface DiContainer extends ReadonlyDiContainer
 {
     function bind(string $id): BindingToSyntax;
