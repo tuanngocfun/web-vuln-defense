f1c32a455757ec154ad7a7f44d8a1c193272bc69
tuanngocfun <tuanngoccs50@gmail.com>
2024-12-29 08:33:34 +0000

    exception was adding

---
+++ src/Core/Exceptions/UnexpectedActionArgumentException.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Core\Exceptions;
+
+class UnexpectedActionArgumentException extends \RuntimeException
+{
+
+}
---
+++ src/Core/Exceptions/ViewRenderException.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Core\Exceptions;
+
+class ViewRenderException extends \RuntimeException
+{
+
+}
