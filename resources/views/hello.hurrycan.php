ba6eecf200c5f086e9ccc4a632c7d3b4df9c8692
tuanngocfun <tuanngoccs50@gmail.com>
2025-02-27 01:51:52 +0700

    Added resources/views/hello.hurrycan.php

---
+++ resources/views/hello.hurrycan.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Hello</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+<body>
+    <h1> Hello {{ $name }} </h1>
+</body>
+
+</html>
