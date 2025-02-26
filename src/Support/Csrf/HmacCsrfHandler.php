5b84c19fb2627a0805668035d8f32e631f51a5f6
tuanngocfun <tuanngoccs50@gmail.com>
2025-02-27 01:51:53 +0700

    Added src/Support/Csrf/HmacCsrfHandler.php

---
+++ src/Support/Csrf/HmacCsrfHandler.php
@@ -0,0 +1,34 @@
+<?php
+namespace App\Support\Csrf;
+
+use App\Constants\Env;
+use App\Utils\Crypto;
+use App\Utils\Randoms;
+
+class HmacCsrfHandler implements CsrfHandler
+{
+    private const SESSION_RANDOM_SEPARATOR = '!';
+    private const HMAC_RANDOM_SEPARATOR = '.';
+
+    public function __construct(private readonly string $csrfSecret) {
+        
+    }
+
+    #[\Override]
+    public function generate(string $data): string {
+        $randomValueLength = 32;
+        $randomValue = Randoms::hexString($randomValueLength);
+        $message = $data . static::SESSION_RANDOM_SEPARATOR . $randomValue;
+        $hmac = Crypto::hash($message, $this->csrfSecret);
+        return $hmac . static::HMAC_RANDOM_SEPARATOR . $randomValue;
+    }
+
+    #[\Override]
+    public function validate(string $csrfToken, string $data): bool {
+        $tokenPartCount = 2;
+        [$hmac, $randomValue] = explode(static::HMAC_RANDOM_SEPARATOR, $csrfToken, $tokenPartCount);
+        $message = $data . static::SESSION_RANDOM_SEPARATOR . $randomValue;
+        $expectedHmac = Crypto::hash($message, $this->csrfSecret);
+        return hash_equals($expectedHmac, $hmac);
+    }
+}
