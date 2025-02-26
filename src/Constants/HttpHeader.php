1db4b81d4a36058b7b4d05e9c003efcf2c892aca
tuanngocfun <tuanngoccs50@gmail.com>
2025-02-27 01:51:52 +0700

    Added src/Constants/HttpHeader.php

---
+++ src/Constants/HttpHeader.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Constants;
+
+final class HttpHeader
+{
+    const AUTHORIZATION = 'Authorization';
+    const ACCEPT = 'Accept';
+    const ACCEPT_LANGUAGE = 'Accept-Language';
+    const ACCESS_CONTROL_ALLOW_CREDENTIALS = 'Access-Control-Allow-Credentials';
+    const ACCESS_CONTROL_ALLOW_HEADERS = 'Access-Control-Allow-Headers';
+    const ACCESS_CONTROL_ALLOW_METHODS = 'Access-Control-Allow-Methods';
+    const ACCESS_CONTROL_ALLOW_ORIGIN = 'Access-Control-Allow-Origin';
+    const ACCESS_CONTROL_EXPOSE_HEADERS = 'Access-Control-Expose-Headers';
+    const ACCESS_CONTROL_MAX_AGE = 'Access-Control-Max-Age';
+    const ACCESS_CONTROL_REQUEST_METHOD = 'Access-Control-Request-Method';
+    const ACCESS_CONTROL_REQUEST_HEADERS = 'Access-Control-Request-Headers';
+    const CONTENT_DISPOSITION = 'Content-Disposition';
+    const CONTENT_TYPE = 'Content-Type';
+    const CONTENT_LANGUAGE = 'Content-Language';
+    const HOST = 'Host';
+    const LOCATION = 'Location';
+    const ORIGIN = 'Origin';
+    const VARY = 'Vary';
+    const X_REQUESTED_WITH = 'X-Requested-With';
+
+    const X_CSRF_TOKEN = 'X-CSRF-Token';
+    const X_XSRF_TOKEN = 'X-XSRF-Token';
+}
