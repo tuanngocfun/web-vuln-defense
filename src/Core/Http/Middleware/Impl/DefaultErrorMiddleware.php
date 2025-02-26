e30423bdf1645fe0032a3d022961adef5b145e28
tuanngocfun <tuanngoccs50@gmail.com>
2025-02-27 01:51:52 +0700

    Added src/Core/Http/Middleware/Impl/DefaultErrorMiddleware.php

---
+++ src/Core/Http/Middleware/Impl/DefaultErrorMiddleware.php
@@ -0,0 +1,20 @@
+<?php
+namespace App\Core\Http\Middleware\Impl;
+
+use App\Core\Exceptions\HttpException;
+use App\Core\Http\Middleware\ErrorMiddleware;
+use App\Core\Http\Request\Request;
+use App\Core\Http\Response\Response;
+use Closure;
+use Exception;
+
+class DefaultErrorMiddleware implements ErrorMiddleware
+{
+    #[\Override]
+    public function handle(Exception $e, Request $request, Closure $next): Response {
+        if ($e instanceof HttpException) {
+            return response()->err($e->getStatusCode(), $e->getMessage());
+        }
+        throw $e;
+    }
+}
