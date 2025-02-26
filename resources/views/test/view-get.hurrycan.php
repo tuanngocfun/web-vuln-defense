0327d8b0aa40c1ebc4d5284f8b4b4850093620ac
tuanngocfun <tuanngoccs50@gmail.com>
2025-02-27 01:51:52 +0700

    Added resources/views/test/view-get.hurrycan.php

---
+++ resources/views/test/view-get.hurrycan.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>View Get</title>
+</head>
+
+<body>
+    <h2>
+        Foo = {{ $_view->get('foo') }}
+    </h2>
+    <h2>
+        Bar = {{ $_view->get('bar') }}
+    </h2>
+</body>
+
+</html>
