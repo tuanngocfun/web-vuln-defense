7ab171085b9fa48084d009b4c930386ab5590aee
tuanngocfun <tuanngoccs50@gmail.com>
2025-02-27 01:51:52 +0700

    Added resources/views/test/code-block.hurrycan.php

---
+++ resources/views/test/code-block.hurrycan.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Code Block</title>
+</head>
+
+<body>
+    <h1>
+        @php
+            $x = 2;
+        @endphp
+        x = {{ $x }}
+    </h1>
+</body>
+
+</html>
