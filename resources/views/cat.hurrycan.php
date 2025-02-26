1340d773408a687e49ea5544a3f7a159a942523f
tuanngocfun <tuanngoccs50@gmail.com>
2025-02-27 01:51:52 +0700

    Added resources/views/cat.hurrycan.php

---
+++ resources/views/cat.hurrycan.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Cat</title>
+</head>
+
+<body>
+    <img src="{{ assets('images/cat-meme-1.png') }}" alt="A cat thinking of eating snack"/>
+</body>
+
+</html>
