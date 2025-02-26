8ed481b66158e5b291bbf8a939e03ece7edc19c8
tuanngocfun <tuanngoccs50@gmail.com>
2025-02-27 01:51:52 +0700

    Added src/Dal/Models/UserModel.php

---
+++ src/Dal/Models/UserModel.php
@@ -0,0 +1,11 @@
+<?php
+namespace App\Dal\Models;
+
+class UserModel
+{
+    public int $id;
+    public string $name;
+    public string $username;
+    public string $password;
+    public string $role;
+}
